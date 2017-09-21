e16c6f639592fc35c96200a4edc41543ff0242af
DESKTOP-QRBNSL0\Tan <sghianu@gmail.com>
2017-09-21 10:44:59 +0200

    finish project

--- Assets/Scripts/Items/Equipment.cs
+++ Assets/Scripts/Items/Equipment.cs
@@ -1,28 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-/* An Item that can be equipped. */
+/* An Item that can be equipped to increase armor/damage. */
 
-[CreateAssetMenu(fileName = "New Equipment", menuName = "Inventory/Equipment")]
+[CreateAssetMenu(fileName = "New Item", menuName = "Inventory/Equipment")]
 public class Equipment : Item {
 
-	public EquipmentSlot equipSlot;	// Slot to store equipment in
-    public SkinnedMeshRenderer mesh;
-    public EquipmentMeshRegion[] coveredMeshRegions;
+	public EquipmentSlot equipSlot;		// What slot to equip it in
+	public int armorModifier;
+	public int damageModifier;
+	public SkinnedMeshRenderer prefab;
 
-	public int armorModifier;		// Increase/decrease in armor
-	public int damageModifier;      // Increase/decrease in damage
-
-	// When pressed in inventory
-	public override void Use()
+	// Called when pressed in the inventory
+	public override void Use ()
 	{
-		base.Use();
-		EquipmentManager.instance.Equip(this);	// Equip it
-		RemoveFromInventory();					// Remove it from inventory
+		EquipmentManager.instance.Equip(this);	// Equip
+		RemoveFromInventory();	// Remove from inventory
 	}
 
 }
 
-public enum EquipmentSlot { Head, Chest, Legs, Weapon, Shield, Feet }
-public enum EquipmentMeshRegion { Legs, Arms, Torso }; // Corresponds to body blendshapes.
+public enum EquipmentSlot { Head, Chest, Legs, Weapon, Shield, Feet}
